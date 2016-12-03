519d03080ac8db439fbfa0af934b8ef9d8bd2cc4
Salim Uddin <salimuddin_01@hotmail.com>
2016-12-03 13:49:45 +0000

    added operators

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const a = document.getElementById('input1');
 const b = document.getElementById('input2');
 const c = document.getElementById('ranOutput');
-
 const btnCalculate = document.getElementById('calculate');
 btnCalculate.addEventListener('click', myFunction);
 
@@ -11,22 +10,42 @@ document.getElementById("reset").onclick = function() {
 }
 
 function myFunction() {
+	const operators = document.getElementById('operators');
+	const operatorValue = operators.options[operators.selectedIndex].value;
+
+	if(isNaN(a.value)|| isNaN(b.value)){
+		c.innerHTML = "Enter Number value";
+		c.style.color = "red";
+	}
+	else if(a.value ==="" || b.value===""){
+		c.innerHTML = "";
+	}
+	else if(a.value >= b.value){
+		c.innerHTML = "Num 2 should be high";
+		c.style.color = "red";
+	}
+	else{
+		let input1=Math.floor(Math.random()*(b.value - a.value))+1; 
+		let input2=Math.floor(Math.random()*(b.value - a.value))+1;
+		//console.log('input1 before: '+input1 + 'input2 before: '+input2);
+		
+		if(operatorValue ==='addition'){
+			c.innerHTML = (input1+' + '+input2 + ' = '+(input1+input2));
+		}
+		if(operatorValue ==='subtraction'){
+			c.innerHTML = (input1+' - '+input2 + ' = '+(input1-input2));
+		}
+		if(operatorValue ==='multiplication'){
+			c.innerHTML = (input1+' * '+input2 + ' = '+(input1*input2));
+		}
+		if(operatorValue ==='division'){
+			while(input2 < input1){
+				input1=Math.floor(Math.random()*(b.value - a.value))+1;
+				input2=Math.floor(Math.random()*(b.value - a.value))+1;
+			}
+			c.innerHTML = (input2+' ÷ '+input1 + ' = '+(input2/input1).toString().match(/^-?\d+(?:\.\d{0|0})?/)[0]);
+		}
+	
+	}
  
-  if(isNaN(a.value)|| isNaN(b.value)){
-	c.innerHTML = "Enter Number value";
-	c.style.color = "red";
-  }
-  else if(a.value ==="" || b.value===""){
-	c.innerHTML = "";
-  }
-  else if(a.value >= b.value){
-	c.innerHTML = "Num 2 should be high";
-  }
-  else{
-	var input1=Math.floor(Math.random()*(b.value - a.value))+1; 
-	var input2=Math.floor(Math.random()*(b.value - a.value))+1;
-	c.innerHTML = (input1+' '+input2);
-	console.log(c);
-  }
- 
-}
+}//end myFunction()
